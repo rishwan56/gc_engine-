02340afec4305bfd7a95ced41190d70007631d47
rishwan56 <wanrish5647@gmail.com>
2026-06-30 22:07:57 +0530

    cmake configuration

---
+++ build/CMakeFiles/gc_engine.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rishwan56/rishwan_program/gc_engine-/main.c" "CMakeFiles/gc_engine.dir/main.c.o" "gcc" "CMakeFiles/gc_engine.dir/main.c.o.d"
+  "/home/rishwan56/rishwan_program/gc_engine-/objects/snekobject.c" "CMakeFiles/gc_engine.dir/objects/snekobject.c.o" "gcc" "CMakeFiles/gc_engine.dir/objects/snekobject.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/gc_engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gc_engine.dir/main.c.o"
+  "CMakeFiles/gc_engine.dir/main.c.o.d"
+  "CMakeFiles/gc_engine.dir/objects/snekobject.c.o"
+  "CMakeFiles/gc_engine.dir/objects/snekobject.c.o.d"
+  "gc_engine"
+  "gc_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gc_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
