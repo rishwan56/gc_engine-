e1e004d28a198ce620870df633d5b78acb8ddb2e
rishwan56 <wanrish5647@gmail.com>
2026-07-14 19:19:59 +0530

    benchmarking

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -41,4 +41,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/gc_engine.dir/DependInfo.cmake"
+  "CMakeFiles/gc_benchmark.dir/DependInfo.cmake"
   )
--- run_benchmarks.py
+++ run_benchmarks.py
@@ -16,11 +16,40 @@ def main():
     if not os.path.exists(build_dir): 
         os.makedirs(build_dir)
 
-    #1. Benchmark
+    #1. Benchmark Reference Counting
     print("[1/2] Compiling Reference Counting...")
     run_cmd("cmake -DUSE_MARK_SWEEP=OFF ..", cwd = build_dir)
     run_cmd("make gc_benchmark", cwd = build_dir)
     print("Running Reference Counting Benchmark ...")
     run_cmd("./gc_benchmark", cwd = build_dir)
 
+    #2. Benchmark mark & Sweep 
+    print("\n[2/2] Compiling Mark & Sweep...") 
+    run_cmd("cmake -DUSE_MARK_SWEEP=ON ..", cwd = build_dir)
+    run_cmd("make gc_benchmark", cwd = build_dir)
+    print("Running Mark & Sweep Benchmark...")
+    run_cmd("./gc_benchmark", cwd = build_dir)
+
+    #3. Read & Compare Results
+    ref_time, ref_mem = read_result(os.path.join(build_dir, "refcount_bench.txt"))
+    ms_time, ms_mem = read_result(os.path.join(build_dir, "marksweep_bench.txt"))
+
+    print("\n" + "="*50)
+    print("             COMPARATVIE ANALAYSIS REPORT")
+    print("="*50)
+    print(f"{'Metric : <25'} | {'Reference Counting :' <20} | {'Mark & Sweep' : <15}")
+    print("-"*50)
+    print(f"{'Execution Time (sec)':<25} | {ref_time:<20.4f} | {ms_time:<15.4f}")   
+    print(f"{'Peak Heap Objects':<25} | {ref_mem:<20} | {ms_mem:<15}")
+    print("="*50)
+    
+
+    if ref_time > 0 and ms_time > 0:
+        ratio = ref_time / ms_time if ref_time > ms_time else ms_time / ref_time    
+        winner = "Mark & Sweep" if ref_time > ms_time else "Reference Counting"     
+        print(f"\nWinner: {winner} is {ratio:.2f}x faster on this workload!")       
+    print("="*50)
+  
+if __name__ == "__main__":
+    main()
 
