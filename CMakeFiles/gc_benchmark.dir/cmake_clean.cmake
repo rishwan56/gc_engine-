4a799b7b64d168ef491a46ce183d6da942088650
rishwan56 <wanrish5647@gmail.com>
2026-07-15 23:12:49 +0530

    update

---
+++ CMakeFiles/gc_benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gc_benchmark.dir/benchmark.c.o"
+  "CMakeFiles/gc_benchmark.dir/benchmark.c.o.d"
+  "CMakeFiles/gc_benchmark.dir/mark_and_sweep/sneknew.c.o"
+  "CMakeFiles/gc_benchmark.dir/mark_and_sweep/sneknew.c.o.d"
+  "CMakeFiles/gc_benchmark.dir/mark_and_sweep/snekobject.c.o"
+  "CMakeFiles/gc_benchmark.dir/mark_and_sweep/snekobject.c.o.d"
+  "CMakeFiles/gc_benchmark.dir/mark_and_sweep/vm.c.o"
+  "CMakeFiles/gc_benchmark.dir/mark_and_sweep/vm.c.o.d"
+  "CMakeFiles/gc_benchmark.dir/stack/stack.c.o"
+  "CMakeFiles/gc_benchmark.dir/stack/stack.c.o.d"
+  "gc_benchmark"
+  "gc_benchmark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gc_benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/gc_benchmark.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gc_benchmark.
+# This may be replaced when dependencies are built.
